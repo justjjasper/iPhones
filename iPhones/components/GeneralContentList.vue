00cc4f32fa7384b86533885faa4228be9594f4c7
JASPER <jasjasper101@gmail.com>
2023-11-30 13:36:52 -0800

    Implement search method

--- iPhones/components/GeneralContentList.vue
+++ iPhones/components/GeneralContentList.vue
@@ -1,28 +1,32 @@
 <template>
-  <section class="general-content-list-container sm:border-0 border-[1.5px] border-black rounded-lg flex flex-col bg-white shadow-lg"
-  @click="handleToggleDropDownList">
+  <section class="general-content-list-container sm:border-0 border-[1.5px] border-black rounded-lg flex flex-col bg-white shadow-lg">
 
     <!-- Mobile DropDownList -->
     <div class="flex items-center justify-between w-full py-3 sm:hidden px-8 cursor-pointer relative"
-    :class="{ 'divider': dropDownList }">
+    :class="{ 'divider': dropDownList }"
+    @click="handleToggleDropDownList">
       <span class="font-bold text-2xl">iPhone Models</span>
       <!-- Plus Circle SVG -->
-      <svg v-if="!dropDownList" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-plus-circle" viewBox="0 0 16 16">
+      <svg v-if="!dropDownList" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
       </svg>
        <!-- Minus Circle SVG -->
-      <svg v-if="dropDownList" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-dash-circle" viewBox="0 0 16 16">
+      <svg v-if="dropDownList" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
         <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
       </svg>
     </div>
 
     <!-- General Content List -->
-    <div class="general-content-list-container overflow-scroll p-6 h-[45vh] rounded-lg sm:block"
+    <div class="general-content-list-container overflow-scroll p-6 h-[45vh] rounded-lg flex-col sm:block"
     :class="{ 'flex': dropDownList, 'hidden': !dropDownList }">
+      <!--Search Component-->
+      <Search :iPhoneModels="iPhoneModels" v-model="searchQuery" @input="handleSearchInput"/>
+
+      <!--Mapped Products-->
       <ul class="general-content-list">
-        <li v-for="model, index in iPhoneModels" :key="index" class="flex flex-col items-center cursor-pointer" @click="handleSelectModel(index)">
+        <li v-for="model, index in filteredModels" :key="index" class="flex flex-col items-center cursor-pointer" @click="handleSelectModel(index)">
           <img class="h-auto w-[190px] transition-transform ease-in-out duration-300 hover:scale-110 transform py-3"
             :src="model.image"
             :alt="`Image of ${model.name}`"/>
@@ -34,9 +38,13 @@
 </template>
 
 <script lang="ts">
-import {type iPhoneModelType} from '../types'
+import type { iPhoneModelType } from '../types'
+import Search from './Search.vue'
 
 export default {
+  components: {
+    Search
+  },
   props: {
     iPhoneModels: {
       type: Array as () => iPhoneModelType[],
@@ -51,13 +59,27 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      searchQuery: '',
+    };
+  },
+  computed: {
+    filteredModels(): iPhoneModelType[] {
+      const query = this.searchQuery.toLowerCase();
+      return this.iPhoneModels.filter((model) => model.name.toLowerCase().includes(query));
+    },
+  },
   methods: {
     handleSelectModel(index: number) {
       this.$emit('selectModel', index)
     },
     handleToggleDropDownList() {
       this.$emit('toggleDropDownList')
-    }
+    },
+    handleSearchInput(searchQuery: string) {
+      this.searchQuery = searchQuery;
+    },
   }
 }
 </script>
@@ -67,10 +89,10 @@ export default {
 @media (min-width: 640px) {
   .general-content-list-container {
     grid-column: 1/span 2
-       /* Same as
-   grid-column-start: 1;
-   grid-column-end: span 2;
-   */
+    /* Same as
+    grid-column-start: 1;
+    grid-column-end: span 2;
+    */
   }
 }
 @media (min-width: 1280px) {
---
+++ iPhones/components/Search.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="my-1 m-auto w-[80%] sm:w-[60%]">
+    <form class="relative flex items-center">
+      <!--Search SVG-->
+        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="23" height="23" viewBox="0 0 50 50" fill='black' class='absolute left-[10px]'>
+        <path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"></path>
+        </svg>
+      <input
+        v-model="searchQuery"
+        type="text"
+        class="border-[1.5px] w-full py-1.5 px-10 rounded-full border-black"
+        placeholder="Search Here"
+        @input="emitSearchQuery"
+        @keydown.enter.prevent="emitSearchQuery"/>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import type { iPhoneModelType } from '~/types'
+
+export default {
+  props: {
+    iPhoneModels: {
+      type: Array as () => iPhoneModelType[],
+      required: true
+    },
+  },
+  data() {
+    return {
+      searchQuery: '',
+    }
+  },
+  methods: {
+    emitSearchQuery() {
+      this.$emit('input', this.searchQuery)
+    }
+  }
+}
+</script>
